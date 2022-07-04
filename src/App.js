3bd5bbff530359a3cdde168f6faffa20cd4eb7c3
Surya Bhakti Kusuma <surya27bk@gmail.com>
2022-07-04 13:29:40 +0700

    button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import Button from "./component/Button";
 import Card from "./component/Card";
 import data from "./data.json";
 console.log(data);
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ position: "relative" }}>
       <Card dataUser={data} />
+      <Button />
     </div>
   );
 }
---
+++ src/component/Button.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./button.css";
+
+function Button() {
+  return <div className="button">Post News</div>;
+}
+
+export default Button;
